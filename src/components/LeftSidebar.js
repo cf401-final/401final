0522228203f6fb84f23baac3145b8178b1abc2ce
kason <kbraley9@gmail.com>
2021-12-20 13:18:19 -0700

    (feat): Add Profile and Loading wrapper to sidebar

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -3,14 +3,20 @@ import RoomChatLeftSidebar from './roomChatLeftSidebar';
 import MatcherLeftSidebar from './roomChatLeftSidebar/MatcherSidebar';
 import LoginButton from './auth/Login';
 import LogoutButton from './auth/Logout';
+import Profile from './auth/Profile';
+import LoadingWrapper from './auth/LoadingWrapper';
 
 import { Route, Routes } from 'react-router-dom';
 
 const LeftSidebar = () => {
   return (
     <div className="left-sidebar" id="resizable" elevation={10}>
-    <LoginButton/>
-    <LogoutButton/>
+      <LoadingWrapper>
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+      </LoadingWrapper>
+
       <Routes>
         <Route path="/roomchat" element={<RoomChatLeftSidebar />}></Route>
         <Route path="/matcher" element={<MatcherLeftSidebar />}></Route>
