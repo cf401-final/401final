7684d7e1cbbb7f56676e8b80f435b7f2c2f17631
kason <kbraley9@gmail.com>
2021-12-20 11:38:56 -0700

    (feat): Use Auth0 login/logout buttons

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import RoomChatLeftSidebar from './roomChatLeftSidebar';
 import MatcherLeftSidebar from './roomChatLeftSidebar/MatcherSidebar';
-import AuthButtons from './AuthButtons';
+import LoginButton from './auth/Login';
+import LogoutButton from './auth/Logout';
 
 import { Route, Routes } from 'react-router-dom';
 
 const LeftSidebar = () => {
   return (
     <div className="left-sidebar" id="resizable" elevation={10}>
+    <LoginButton/>
+    <LogoutButton/>
       <Routes>
-        <Route path="/" element={<AuthButtons />}></Route>
         <Route path="/roomchat" element={<RoomChatLeftSidebar />}></Route>
         <Route path="/matcher" element={<MatcherLeftSidebar />}></Route>
       </Routes>
