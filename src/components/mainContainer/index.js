6f6d46c9f5cb65315b4d44bf20ba5cbe1c786836
Ryan Emmans <ryanemmans@gmail.com>
2021-12-20 20:25:32 -0800

    Adds scrolling to chat, improves message bar visibility.

--- src/components/mainContainer/index.js
+++ src/components/mainContainer/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Typography } from '@mui/material';
 
 const MainContainer = ({ children }) => {
-  return <div className="main-container">MAIN CONTAINER{children}</div>;
+  return (
+    <>
+      <div className="main-container">
+        <Typography
+          variant="button"
+          display="block"
+          gutterBottom
+          className="roomTitle"
+          sx={{ml: 1}}
+        >
+          Name of Chat Room / Feature
+        </Typography>
+        {children}
+      </div>
+    </>
+  )
 };
 
 export default MainContainer;
