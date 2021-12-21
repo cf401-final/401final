3485ed79801e8d620f986b2e0307380fa14b3e33
Ryan Emmans <ryanemmans@gmail.com>
2021-12-20 16:35:59 -0800

    Adjusts sidebar layout, updates landing page, adds auth loading spinner, installs MUI lab dependency.

--- src/components/auth/LoadingWrapper.js
+++ src/components/auth/LoadingWrapper.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 function LoadingWrapper({ children }) {
   const { isLoading, error } = useAuth0();
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div><LoadingButton loading variant="contained" color="error"/></div>;
   }
   if (error) {
     return <div>Oops... {error.message}</div>;
