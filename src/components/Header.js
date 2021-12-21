6d73a7cd19192ca10ff9b092a2989a2b8d0ab9aa
kason <kbraley9@gmail.com>
2021-12-21 11:41:53 -0700

    Move login/logout button to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,12 @@ import { Typography, Button } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 import github from '../img/github.png';
+import LoginButton from './auth/Login';
+import LogoutButton from './auth/Logout';
+import Profile from './auth/Profile';
+import LoadingWrapper from './auth/LoadingWrapper';
 
 const theme = createTheme({
   palette: {
@@ -21,19 +26,37 @@ const Header = () => {
           <AccountCircleIcon sx={{ fontSize: 25 }} />
         </Button>
 
+        <LoadingWrapper>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </LoadingWrapper>
+
         <h1>
-          <ForumOutlinedIcon id="bubble" />Jangle
+          <ForumOutlinedIcon id="bubble" />
+          Jangle
         </h1>
 
-        <Typography id="onlineUsers" variant="subtitle2" gutterBottom component="div">12,345 users currently online.</Typography>
-        <a href="https://github.com/jangle401/jangle-front" target="_blank" rel="noreferrer">
+        <Typography
+          id="onlineUsers"
+          variant="subtitle2"
+          gutterBottom
+          component="div"
+        >
+          12,345 users currently online.
+        </Typography>
+        <a
+          href="https://github.com/jangle401/jangle-front"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Button id="githubBtn" variant="contained" color="primary">
-          <img className="btnImg" src={github} />
-        </Button>
+            <img className="btnImg" src={github} />
+          </Button>
         </a>
       </ThemeProvider>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
