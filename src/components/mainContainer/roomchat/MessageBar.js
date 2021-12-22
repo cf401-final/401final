2f5d6906036237fa5a4212fc35db65888686c055
kason <kbraley9@gmail.com>
2021-12-22 13:50:14 -0700

    (fix): Remove duplicate room join code

--- src/components/mainContainer/roomchat/MessageBar.js
+++ src/components/mainContainer/roomchat/MessageBar.js
@@ -1,19 +1,9 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Paper, InputBase } from '@mui/material';
 import { SocketContext } from '../../../context/socket';
-import { useAuth0 } from '@auth0/auth0-react';
 
 const MessageBar = () => {
   const { socket, currentRoom } = useContext(SocketContext);
-  const { isAuthenticated, user } = useAuth0();
-
-  let username = isAuthenticated
-    ? user.nickname
-    : `Test-User#${Math.round(Math.random() * 1000)}`;
-
-  useEffect(() => {
-    socket.emit('join', { room: currentRoom, username });
-  }, [socket, currentRoom, username]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,7 +15,6 @@ const MessageBar = () => {
 
   return (
     <div className="message-bar">
-      {/* <form> */}
       <Paper
         className="msgPaper"
         onSubmit={handleSubmit}
@@ -45,8 +34,6 @@ const MessageBar = () => {
           inputProps={{ 'aria-label': 'send message' }}
         />
       </Paper>
-      {/* <TextField className="msgInput" name="message" variant="standard" /> */}
-      {/* </form> */}
     </div>
   );
 };
