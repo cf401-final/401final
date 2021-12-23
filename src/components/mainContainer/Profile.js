d43ea4d13bfdde592f3ba578f830bbbe36f7a27c
Beers15 <alexanderbeers15@gmail.com>
2021-12-23 11:25:18 -0800

    Grab profile values from form on submittal.

--- src/components/mainContainer/Profile.js
+++ src/components/mainContainer/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Paper, Typography, ToggleButtonGroup, ToggleButton, TextField, Button } from '@mui/material';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import CheckIcon from '@mui/icons-material/Check';
@@ -28,6 +29,7 @@ const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
 }));
 
 const Profile = () => {
+  const { user } = useAuth0();
   const [selected, setSelected] = React.useState(false);
   const interests = [
     {label: 'Music', value: 'music'}, {label: 'Crafts & DIY', value: 'craftsdiy'},
@@ -42,6 +44,12 @@ const Profile = () => {
 
   const handleSelected = (event, newSelection) => {
     setSelected(newSelection);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let body = { interests: selected, bio: e.target.bio.value, user: user.nickname };
+    console.log(body)
   };
 
   return (
@@ -61,39 +69,47 @@ const Profile = () => {
             <h3>IMAGE GOES HERE</h3>
             <p>Choose Some Interests:</p>
           </Typography>
-          <StyledToggleButtonGroup
-            id="toggleGroup"
-            size="small"
-            // color="primary"
-            value={selected}
-            aria-label="text formatting"
-            onChange={handleSelected}
-          >
-            {interests.map(interest => {
-              return (
-                <ToggleButton key={interest.value} className="interestBtn" color="primary" value={interest.value} aria-label={interest.value}>
-                  {interest.label}
-                </ToggleButton>
-              )
-            })}
-          </StyledToggleButtonGroup>
-          <TextField
-            color="primary"
-            id="outlined-multiline-static"
-            label="Enter a Bio:"
-            multiline
-            rows={5}
-            placeholder="Tell other users about yourself..."
-          />
-          <div className="profileRow">
-            <Button
-              className="updateBtn"
-              size="large"
-              variant="contained"
+          <form onSubmit={handleSubmit}>
+            <StyledToggleButtonGroup
+              id="toggleGroup"
+              size="small"
+              // color="primary"
+              value={selected}
+              aria-label="text formatting"
+              onChange={handleSelected}
+            >
+              {interests.map(interest => {
+                return (
+                  <ToggleButton key={interest.value} className="interestBtn" color="primary" value={interest.value} aria-label={interest.value}>
+                    {interest.label}
+                  </ToggleButton>
+                )
+              })}
+            </StyledToggleButtonGroup>
+            <TextField
               color="primary"
-              endIcon={<CheckIcon />}>Update
-            </Button>
-          </div>
+              required
+              id="outlined-multiline-static"
+              name="bio"
+              label="Enter a Bio:"
+              multiline
+              rows={5}
+              sx={{ width: '100%'}}
+              placeholder="Tell other users about yourself..."
+            />
+            <div className="profileRow">
+              <Button
+                className="updateBtn"
+                size="large"
+                variant="contained"
+                color="primary"
+                endIcon={<CheckIcon />}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </div>
+          </form>
         </Paper>
       </ThemeProvider>
     </div >
