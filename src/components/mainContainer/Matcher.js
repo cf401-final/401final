07a89c2901c83c26c8c58ed4e47591d89ea89d03
Beers15 <alexanderbeers15@gmail.com>
2021-12-23 14:09:02 -0800

    Add unstyled container+button to the Matcher page that grabs a random profile from the API server when clicked.

--- src/components/mainContainer/Matcher.js
+++ src/components/mainContainer/Matcher.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import { Paper, Button, Typography } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
 
 const Matcher = () => {
-  return <div>I AM MATCHER</div>;
+  const { user } = useAuth0();
+  const [selected, setSelected] = React.useState([]);
+  const [bio, setBio] = React.useState('');
+  const [username, setUsername] = React.useState('');
+
+  const getRandomUser = async () => {
+    try {
+      let res = await axios.get(`${process.env.REACT_APP_API_SERVER}/profiles/${user.nickname}/random`);
+      if(res.data) {
+        setSelected(res.data.interests);
+        setBio(res.data.bio);
+        setUsername(res.data.username);
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div id="profile">
+      <Paper
+        id="profilePaper"
+        elevation={0}
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+        }}
+      >
+        {selected.map(interest => {
+          return <Typography key={interest}>{interest}</Typography>
+        })}
+        <Typography>{bio}</Typography>
+        <Typography>{username}</Typography>
+          
+        <Button
+          className="updateBtn"
+          size="large"
+          variant="contained"
+          color="success"
+          endIcon={<CheckIcon />}
+          onClick={getRandomUser}
+        >
+          Get Random User
+        </Button>
+      </Paper>
+    </div>
+  );
 };
 
 export default Matcher;
