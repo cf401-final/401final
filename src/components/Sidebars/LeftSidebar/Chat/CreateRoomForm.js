a9d7f814e8fdb57f78c8ef1f5a5bf3b6ccfb1c78
Beers15 <alexbeerz@yahoo.com>
2021-12-22 16:09:09 -0800

    Add styled alert when adding rooms to server fails for any reason.

--- src/components/Sidebars/LeftSidebar/Chat/CreateRoomForm.js
+++ src/components/Sidebars/LeftSidebar/Chat/CreateRoomForm.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { MenuItem, TextField, Button, Typography, Divider } from '@mui/material';
+import axios from 'axios';
+import swal from 'sweetalert';
 
 const CreateRoomForm = ({ handleClose }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(e.target.roomname.value);
-    console.log(e.target.password.value);
+    await addRoomToServer(e.target.roomname.value, e.target.password.value);
     handleClose();
+  }
+
+  const addRoomToServer = async (roomname, password) => {
+    let body = password ? { roomname, password } : { roomname };
+    let res = null;
+    try {
+      res = await axios.post(`${process.env.REACT_APP_API_SERVER}/rooms`, body);
+      console.log(res.data);
+    } catch(err) {
+      console.log(err);
+      swal({
+        title: "Oh no!",
+        text: "Unable to create a room with that name.",
+        icon: "warning",
+        dangerMode: true,
+      });
+    }
   }
 
   return (
