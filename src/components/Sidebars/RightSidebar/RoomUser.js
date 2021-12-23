56df3a864d657ebdae719a94aaa0356a91eba22d
Beers15 <alexbeerz@yahoo.com>
2021-12-22 18:12:02 -0800

    1-1 rooms can be created by click on a user in right sidebar userlist.

--- src/components/Sidebars/RightSidebar/RoomUser.js
+++ src/components/Sidebars/RightSidebar/RoomUser.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box, Tooltip, Button } from '@mui/material';
+import axios from 'axios';
 import swal from 'sweetalert';
 import { useAuth0 } from '@auth0/auth0-react';
+import { SocketContext } from '../../../context/socket';
 
 const UserButton = ({ username }) => {
   const { user } = useAuth0();
+  const { socket, setCurrentRoom } = useContext(SocketContext);
+
+
+  const createDirectMessageRoom = async () => {
+    let roomname = `${user.nickname}-${username}`;
+    let body = { roomname, users: [user.nickname, username]};
+
+    try {
+      await axios.post(`${process.env.REACT_APP_API_SERVER}/rooms`, body);
+      setCurrentRoom(roomname);
+      try {
+        socket.emit('join', {
+          room: roomname,
+          user: user.nickname,
+        });
+        setCurrentRoom(roomname);
+      } catch (err) {
+        console.log(err);
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
 
   const handleClick = async () => {
     if(username === user.nickname) {
@@ -14,8 +39,9 @@ const UserButton = ({ username }) => {
       });
       return;
     }
+    await createDirectMessageRoom();
   }
-
+  
   return (
     <>
       <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
