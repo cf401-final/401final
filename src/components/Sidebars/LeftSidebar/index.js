1baa9266c9c5967f8d2fbe817e6994c7a957a058
Ryan Emmans <ryanemmans@gmail.com>
2021-12-27 12:58:49 -0800

    Adds nav buttons to left sidebar footer.

--- src/components/Sidebars/LeftSidebar/index.js
+++ src/components/Sidebars/LeftSidebar/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import Nav from './Nav';
 import ChatSidebar from './Chat';
 import MatcherSidebar from './Matcher/MatcherSidebar';
+import { Typography } from '@mui/material';
 
 import { Route, Routes } from 'react-router-dom';
 
 const LeftSidebar = () => {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div className="left-sidebar" id="resizable" elevation={10}>
-      <Routes>
-        <Route path="/roomchat" element={<ChatSidebar />} />
-        <Route path="/matcher" element={<MatcherSidebar />} />
-      </Routes>
-      <p className="footer">© Jangle 2021</p>
-    </div>
+    isAuthenticated ? (
+      <div className="left-sidebar" id="resizable" elevation={10}>
+        <Routes>
+          <Route path="/roomchat" element={<ChatSidebar />} />
+          <Route path="/matcher" element={<MatcherSidebar />} />
+        </Routes>
+        <div className="footer">
+          <Nav />
+          <Typography variant="overline" gutterBottom style={{ textAlign: 'center', marginTop: '1.4rem' }}>© Jangle 2021</Typography>
+        </div>
+      </div>
+    ) : (
+      <div className="left-sidebar" id="resizable" elevation={10}>
+        <Typography variant="overline" gutterBottom className="footer" style={{ textAlign: 'center', marginTop: '1.4rem' }}>© Jangle 2021</Typography>
+      </div>
+    )
   );
 };
 
